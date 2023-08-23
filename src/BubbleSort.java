71e41571a5afafd10d7dc55e5ae81021f79e52f7
Lourdene Eira Sanots <2233120@slu.edu.ph>
2023-08-23 18:15:33 +0800

    Added appropriate comments in BubbleSort class

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -13,17 +13,40 @@
  * </p>
  */
 
+ /**
+  * The BubbleSort class provides an implementation of the Bubble Sort algorithm for sorting arrays of strings
+  * read from a CSV file. It includes methods for sorting, displaying statement counts, and user interaction.
+  */
  public class BubbleSort {
     public static void main(String[] args) {
+        /*
+        TODO: Add code statements for the main method here.
         
+        Initialize the variables and  get 
+        user's choices for sorting unit and case
+        
+        */ 
     } // end of main method
 
-    //TODO: 
-    public static int sort(){
-
+    /**
+     * Sorts the given array using the Bubble Sort algorithm.
+     * @param arr array of Strings to be sorted.
+     * @return int count of statements executed in the method.
+     */
+    public static int sort(String arr){
+        /*
+            ALGORITHM:
+            1. Read data from the CSV file into an array.
+            2. Initialize statementCount to 0.
+            3. Iterate through the array using Bubble Sort algorithm
+            4. Return the statementCount variable.
+        */
+         
+        //TODO: Add code statements for the bubble sort algorithm here.
     } // end of sort method
 
     public static void displayCounts(){
-
+        //TODO: Add code statements for the displayCounts method here.
+        // Display statement counts using appropriate units
     } // end of displayCounts method
  } // end of BubbleSort class
