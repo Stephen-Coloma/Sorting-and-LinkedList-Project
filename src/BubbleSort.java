8708833f5a403510c9889518c86c64b7ddcd25fe
Hannah Ragudos <hannahragudos@gmail.com>
2023-08-24 18:21:30 +0800

    Completed the documentation for the InsertionSort class

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,13 +3,13 @@
  * Class Code and Course Number: 9342 - CS 211
  * Schedule: TF 9:00 - 10:30 AM
  * <p>
- *     STEPHEN M. COLOMA- 2232847@slu.edu.ph
- *     SANCHIE EARL M. GUZMAN- 2232886@slu.edu.ph
- *     MARIUS GLENN NONATO- 2232731@slu.edu.ph
- *     JERWIN KYLE R. RAMOS- 2232862@slu.edu.ph
- *     HANNAH T. RAGUDOS- 2233361@slu.edu.ph
- *     REY DANIEL L. ROQUE- 2233357@slu.edu.ph
- *     LOURDENE EIRA C. SANTOS- 2233120@slu.edu.ph
+ *     COLOMA, Stephen M.- 2232847@slu.edu.ph
+ *     GUZMAN, Sanchie Earl M.- 2232886@slu.edu.ph
+ *     NONATO, Marius Glenn M.- 2232731@slu.edu.ph
+ *     RAMOS, Jerwin Kyle R.- 2232862@slu.edu.ph
+ *     RAGUDOS, Hannah T.- 2233361@slu.edu.ph
+ *     ROQUE, Rey Daniel L. - 2233357@slu.edu.ph
+ *     SANTOS, Lourdene Eira C.- 2233120@slu.edu.ph
  * </p>
  */
 
