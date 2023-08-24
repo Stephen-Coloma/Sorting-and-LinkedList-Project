61d1fd481e703a5cac22c13b5cd1c99ced19dfc9
Sanchie Earl Guzman <2232886@slu.edu.ph>
2023-08-24 10:51:29 +0800

    Added the neccessary codes inside the main method of BubblSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -13,19 +13,43 @@
  * </p>
  */
 
- /**
+import java.util.ArrayList;
+
+/**
   * The BubbleSort class provides an implementation of the Bubble Sort algorithm for sorting arrays of strings
   * read from a CSV file. It includes methods for sorting, displaying statement counts, and user interaction.
   */
  public class BubbleSort {
     public static void main(String[] args) {
-        /*
-        TODO: Add code statements for the main method here.
+        public static void main(String[] args) {
+            /*ALGORITHM:
+             * 1. Initiate the 2D array of length 5 by 3.
+             * 2. The row will be the size variant (10k,50k,200k,500k,1M)
+             * 3. The column will be the type arrangement variant (Best, Worst, Average)
+             * 4. Create a list of strings that stores the file path above. Follow format BEST-WORST-AVERAGE
+             * 5. Initiate variable int fileNumber = 0, it is the position of Best10k in the list
+             * 6. Create a double for loop that populates the 2D array.
+             * 7. Call the displayCount method and pass in the 2D array.
+             * */
 
-        Initialize the variables and  get
-        user's choices for sorting unit and case
+            int sizeVariants = 5;
+            int arrangementVariants = 3;
 
-        */
+            long[][] results = new long[sizeVariants][arrangementVariants];
+
+            ArrayList<String> filePathInList= createFilePathInList();
+
+            int fileNumber = 0;
+
+            for (int i = 0; i < sizeVariants; i++) {
+                for (int j = 0; j < arrangementVariants; j++) {
+                    results[i][j] = insertionSort(filePathInList.get(fileNumber));
+                    fileNumber++;
+                    System.out.println("File: " + fileNumber);
+                }
+            }
+
+            displayCounts(results);
     } // end of main method
 
     /**
