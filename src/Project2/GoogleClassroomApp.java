3b2e6013a639bcc6cee8d97676c5a1d1c9442dab
MARIUS GLENN NONATO <2232731@slu.edu.ph>
2023-09-17 02:29:44 +0800

    added course: Operating Systems

--- src/Project2/GoogleClassroomApp.java
+++ src/Project2/GoogleClassroomApp.java
@@ -18,7 +18,6 @@ public class GoogleClassroomApp extends JFrame {
     private void run() {
         //initial population of Courses, Terms and Topics
         courseList = initialPopulation();
-
         this.setContentPane(new CourseListPage(courseList));
         this.setTitle("Course List Page");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -70,13 +69,53 @@ public class GoogleClassroomApp extends JFrame {
         finalsC1.insert(topic1FinalsC1);
         finalsC1.insert(topic1FinalsC1);
 
-        //FOR THE SECOND COURSE: PWEDE NYO DAGDAGAN HERE NG PARANG ASA TAAS
+        //FOR THE SECOND COURSE:
+        Course c2 = new Course<>("OPERATING SYSTEMS" , "9343 - CS 212");
 
+        Topic topic1PrelimsC2 = new Topic("Introduction to Operating Systems", "Task Name Here");
+        Topic topic2PrelimsC2 = new Topic("Process Management", "Task Name Here");
+        Topic topic3PrelimsC2 = new Topic("Memory Management", "Task Name Here");
+        Topic topic4PrelimsC2 = new Topic("File Systems and Storage Management", "Task Name Here");
+        Topic topic5PrelimsC2 = new Topic("I/O Systems and Device Management", "Task Name Here");
 
+        Topic topic1MidtermsC2 = new Topic("Process Synchronization", "Task Name Here");
+        Topic topic2MidtermsC2 = new Topic("CPU Scheduling and Multiprogramming", "Task Name Here");
+        Topic topic3MidtermsC2 = new Topic("Virtual Memory Management", "Task Name Here");
+        Topic topic4MidtermsC2 = new Topic("File System Implementation", "Task Name Here");
+        Topic topic5MidtermsC2 = new Topic("I/O Systems Optimization", "Task Name Here");
+
+        Topic topic1FinalsC2 = new Topic("Process and Thread Management", "Task Name Here");
+        Topic topic2FinalsC2 = new Topic("Memory Protection and Security", "Task Name Here");
+        Topic topic3FinalsC2 = new Topic("Graph Search/Traversal", "Task Name Here");
+        Topic topic4FinalsC2 = new Topic("Security and Authentication", "Task Name Here");
+        Topic topic5FinalsC2 = new Topic("Virtualization and Cloud Computing", "Task Name Here");
+
+        Term prelimsC2 = new Term<>("Prelims");
+        Term midtermsC2 = new Term<>("Midterms");
+        Term finalsC2 =  new Term<>("Midterms");
+
+        prelimsC2.insert(topic1PrelimsC1);
+        prelimsC2.insert(topic2PrelimsC1);
+        prelimsC2.insert(topic3PrelimsC1);
+        prelimsC2.insert(topic4PrelimsC1);
+        prelimsC2.insert(topic5PrelimsC1);
+
+        midtermsC2.insert(topic1MidtermsC1);
+        midtermsC2.insert(topic2MidtermsC1);
+        midtermsC2.insert(topic3MidtermsC1);
+        midtermsC2.insert(topic3MidtermsC1);
+        midtermsC2.insert(topic3MidtermsC1);
+
+        finalsC2.insert(topic1FinalsC1);
+        finalsC2.insert(topic1FinalsC1);
+        finalsC2.insert(topic1FinalsC1);
+        finalsC2.insert(topic1FinalsC1);
+        finalsC2.insert(topic1FinalsC1);
 
         //--------------------------------------------------
         DoublyLinkedList<Course> courseList = new DoublyLinkedList<>();
         courseList.insert(c1);
+        courseList.insert(c2);
 
         return courseList;
     }
