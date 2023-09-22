ed0973acd0863a17a7892111b9e2c2b80ea4dbe5
Sanchie Earl Guzman <2232886@slu.edu.ph>
2023-09-22 12:09:04 +0800

    Fixed errors in the TermPage class

--- src/Project2/GUI/TermPage.java
+++ src/Project2/GUI/TermPage.java
@@ -1,3 +1,23 @@
+/**
+ * Group MixAndMatch
+ * Class Code and Course Number: 9342 - CS 211
+ * Schedule: TF 9:00 - 10:30 AM
+ * <p>
+ *     COLOMA, Stephen M.- 2232847@slu.edu.ph
+ *     GUZMAN, Sanchie Earl M.- 2232886@slu.edu.ph
+ *     NONATO, Marius Glenn M.- 2232731@slu.edu.ph
+ *     RAGUDOS, Hannah T.- 2233361@slu.edu.ph
+ *     RAMOS, Jerwin Kyle R.- 2232862@slu.edu.ph
+ *     ROQUE, Rey Daniel L. - 2233357@slu.edu.ph
+ *     SANTOS, Lourdene Eira C.- 2233120@slu.edu.ph
+ * </p>
+ */
+
+// TODO: Provide the algorithm here:
+/*
+ALGORITHM:
+ */
+
 package Project2.GUI;
 
 import Project2.LinkedListImplementation.DoublyLinkedList;
