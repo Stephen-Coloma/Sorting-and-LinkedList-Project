498ccf94d3bb776837658d5364680f2ddf415599
Sanchie Earl Guzman <2232886@slu.edu.ph>
2023-09-22 12:45:08 +0800

    Added buttons to the EditTopicPage class

--- src/Project2/GUI/EditTopicPage.java
+++ src/Project2/GUI/EditTopicPage.java
@@ -3,7 +3,7 @@ import Project2.ReferenceClasses.Topic;
 import javax.swing.*;
 import java.awt.*;
 
-public class EditTopicPage {
+public class EditTopicPage extends JFrame{
     private JTextField moduleField, taskField;
     private JButton updateButton;
     private JPanel editTopicPanel, buttonPanel;
@@ -13,5 +13,24 @@ public class EditTopicPage {
         setTitle("Edit Topic");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        // Initialize and populate input fields with topic details
+        moduleField = new JTextField(selectedTopic.getModule());
+        taskField = new JTextField(selectedTopic.getTask());
+
+        updateButton = new JButton("Update");
+        updateButton.addActionListener(e -> {
+            // Update the topic details based on the input fields
+            selectedTopic.setModule(moduleField.getText());
+            selectedTopic.setTask(taskField.getText());
+            topicDefaultListModel.setElementAt(selectedTopic, selectedIndex);
+
+            //Debugging: to determine if the topic is indeed edited
+            System.out.println(selectedTopic.getModule());
+            System.out.println(selectedTopic.getTask());
+
+            // Close the edit page
+            dispose();
+
+        });
     }
 }
