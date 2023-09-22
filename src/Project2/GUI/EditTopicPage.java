e925f74db99e0984984af07f75869df039a7d4c3
Jerwin Kyle Ramos <2232682@slu.edu.ph>
2023-09-22 12:48:37 +0800

    Added methods in EditTopicPage class

--- src/Project2/GUI/EditTopicPage.java
+++ src/Project2/GUI/EditTopicPage.java
@@ -1,4 +1,17 @@
 package Project2.GUI;
+import Project2.ReferenceClasses.Topic;
+import javax.swing.*;
+import java.awt.*;
 
 public class EditTopicPage {
+    private JTextField moduleField, taskField;
+    private JButton updateButton;
+    private JPanel editTopicPanel, buttonPanel;
+
+    public EditTopicPage(DefaultListModel<Topic> topicDefaultListModel, Topic selectedTopic, int selectedIndex) {
+
+        setTitle("Edit Topic");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
 }
