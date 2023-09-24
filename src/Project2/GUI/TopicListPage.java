b1f40313633ba26b3a78929279074889df5db79d
Hannah Ragudos <2233361@slu.edu.ph>
2023-09-24 16:24:14 +0800

    Designed the TopicsListPage

--- src/Project2/GUI/TopicListPage.java
+++ src/Project2/GUI/TopicListPage.java
@@ -1,23 +1,3 @@
-/**
- * Group MixAndMatch
- * Class Code and Course Number: 9342 - CS 211
- * Schedule: TF 9:00 - 10:30 AM
- * <p>
- *     COLOMA, Stephen M.- 2232847@slu.edu.ph
- *     GUZMAN, Sanchie Earl M.- 2232886@slu.edu.ph
- *     NONATO, Marius Glenn M.- 2232731@slu.edu.ph
- *     RAGUDOS, Hannah T.- 2233361@slu.edu.ph
- *     RAMOS, Jerwin Kyle R.- 2232862@slu.edu.ph
- *     ROQUE, Rey Daniel L. - 2233357@slu.edu.ph
- *     SANTOS, Lourdene Eira C.- 2233120@slu.edu.ph
- * </p>
- */
-
-// TODO: Provide the algorithm here:
-/*
-ALGORITHM:
- */
-
 package Project2.GUI;
 
 import Project2.GUI.UtilGUI.AddTopicPage;
@@ -38,9 +18,24 @@ public class TopicListPage extends JPanel {
     private JList<Topic> topicsList;
     private Term<Topic> selectedTerm;
 
+    // Custom cell renderer to change the background color of the selected item
+    class CustomListCellRenderer extends DefaultListCellRenderer {
+        @Override
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            if (isSelected) {
+                c.setBackground(new Color(255, 219, 87));
+            } else {
+                c.setBackground(list.getBackground());
+            }
+            return c;
+        }
+    }
+
     public TopicListPage(Term<Topic> term) {
+
         // Construct components
-        topicsLabel = new JLabel("Topics for " + term.getTermName());
+        topicsLabel = new JLabel("TOPICS FOR" + term.getTermName());
         topicsList = new JList<>(new DefaultListModel<>());
         selectedTerm = term;
 
@@ -57,24 +52,91 @@ public class TopicListPage extends JPanel {
         editButton = new JButton("Edit");
         deleteButton = new JButton("Delete");
 
-
         // Adjust size and set layout
         setPreferredSize(new Dimension(452, 457));
         setLayout(null);
 
+        // Modify appearance
+        setBackground(new Color(17, 41, 107));
+        topicsLabel.setFont(new Font("", Font.BOLD, 20));
+        topicsLabel.setForeground(new Color(255, 219, 87));
+
+        Color buttonBgColor = new Color(237, 237, 237);
+
+        // Modify buttons' appearance
+        addButton.setFont(new Font("Roboto", Font.BOLD, 14));
+        addButton.setBorder(BorderFactory.createLineBorder(new Color(255, 219, 87), 2));
+        addButton.setBackground(buttonBgColor);
+        addButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                addButton.setBackground(new Color(255, 219, 87));
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                addButton.setBackground(buttonBgColor);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                addButton.setBackground(buttonBgColor);
+            }
+        });
+
+        editButton.setFont(new Font("Roboto", Font.BOLD, 14));
+        editButton.setBorder(BorderFactory.createLineBorder(new Color(255, 219, 87), 2));
+        editButton.setBackground(buttonBgColor);
+        editButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                editButton.setBackground(new Color(255, 219, 87));
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                editButton.setBackground(buttonBgColor);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                editButton.setBackground(buttonBgColor);
+            }
+        });
+
+        deleteButton.setFont(new Font("Roboto", Font.BOLD, 14));
+        deleteButton.setBorder(BorderFactory.createLineBorder(new Color(255, 219, 87), 2));
+        deleteButton.setBackground(buttonBgColor);
+        deleteButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                deleteButton.setBackground(new Color(255, 219, 87));
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                deleteButton.setBackground(buttonBgColor);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                deleteButton.setBackground(buttonBgColor);
+            }
+        });
+
+        //set component bounds (only needed by Absolute Positioning)
+        topicsList.setBounds(25, 60, 400, 330);
+        topicsList.setBorder(BorderFactory.createLineBorder(new Color(255, 219, 87), 2));
+
         // Add components
+        topicsLabel.setBounds (30, 15, 130, 35);
+        searchBar.setBounds (245, 20, 180, 25);
+        scrollPane.setBounds (25, 60, 400, 330);
+
+        addButton.setBounds(24, 406, 120, 35);
+        editButton.setBounds(165, 406, 120, 35);
+        deleteButton.setBounds(307, 406, 120, 35);
+
         add(topicsLabel);
         add(searchBar);
         add(scrollPane);
         add(addButton);
         add(editButton);
         add(deleteButton);
-        topicsLabel.setBounds (30, 15, 130, 35);
-        searchBar.setBounds (245, 20, 180, 25);
-        scrollPane.setBounds (25, 60, 400, 330);
-        addButton.setBounds (75, 405, 90, 35);
-        editButton.setBounds (180, 405, 90, 35);
-        deleteButton.setBounds(285, 405, 90, 35);
 
         // Double-click on list event
         topicsList.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -89,8 +151,10 @@ public class TopicListPage extends JPanel {
             }
         });
 
-        //add button implementation
-        addButton.addActionListener(e-> {
+        topicsList.setCellRenderer(new TopicListPage.CustomListCellRenderer());
+
+        // Add button implementation
+        addButton.addActionListener(e -> {
             try {
                 AddTopicPage addTopicFrame = new AddTopicPage(selectedTerm, (DefaultListModel<Topic>) topicsList.getModel());
                 addTopicFrame.setVisible(true);
@@ -99,7 +163,7 @@ public class TopicListPage extends JPanel {
             }
         });
 
-        //delete button implementation
+        // Delete button implementation
         deleteButton.addActionListener(e -> {
             try {
                 int selectedIndex = topicsList.getSelectedIndex();
@@ -107,8 +171,7 @@ public class TopicListPage extends JPanel {
                     Topic selectedTopic = topicListModel.getElementAt(selectedIndex);
                     int option = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete the selected Topic?", "Delete Topic", JOptionPane.YES_NO_OPTION);
                     if (option == JOptionPane.YES_OPTION) {
-                        // User confirmed deletion, remove the topic both from the list display and the data structure
-                        topicListModel.remove(selectedIndex);   //If you want to debug, remove this code and delete the selected topic twice to determine if the topic was also removed from the list
+                        topicListModel.remove(selectedIndex);
                         selectedTerm.delete(selectedTopic);
                     }
                 } else {
@@ -119,20 +182,16 @@ public class TopicListPage extends JPanel {
             }
         });
 
-
-        //edit button implementation
+        // Edit button implementation
         editButton.addActionListener(e -> {
             try {
                 int selectedIndex = topicsList.getSelectedIndex();
                 if (selectedIndex >= 0) {
                     Topic selectedTopic = topicListModel.getElementAt(selectedIndex);
 
-                    // Open the edit page for the selected course
                     EditTopicPage editTopicPage = new EditTopicPage(topicListModel, selectedTopic, selectedIndex);
-
-            } else {
-                // If no course selected, show an error message or do nothing
-                JOptionPane.showMessageDialog(this, "Please select a course to edit.", "Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Please select a course to edit.", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(TopicListPage.this, "Error editing topic: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
@@ -141,14 +200,13 @@ public class TopicListPage extends JPanel {
 
         // Add a FocusListener to clear the text when focused
         searchBar.addFocusListener(new FocusAdapter() {
-
             @Override
             public void focusGained(FocusEvent e) {
                 if ("Search".equals(searchBar.getText())) {
                     searchBar.setText("");
                 }
+                searchBar.setBorder(BorderFactory.createLineBorder(new Color(255, 219, 87), 2));
             }
-
             @Override
             public void focusLost(FocusEvent e) {
                 if (searchBar.getText().isEmpty()) {
@@ -190,6 +248,12 @@ public class TopicListPage extends JPanel {
     private void openModulesTasksPage(Topic topic) {
         JFrame frame = new JFrame(topic.getModule()); // Use getModule to set the frame title
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Add the icon to the JFrame
+        String iconPath = "D:\\cs211_9342_mixandmatch\\src\\Project2\\GUI\\Icons\\COURSE ICON.jpg";
+        ImageIcon icon = new ImageIcon(iconPath);
+        frame.setIconImage(icon.getImage());
+
         frame.getContentPane().add(new ModuleTaskPage(topic));
         frame.pack();
         frame.setLocationRelativeTo(null);
